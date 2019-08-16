a391bf795d31b8745f7bed0358ea16ef1d9f9964
SethWickham <seth.wickham@hotmail.com>
2019-08-16 14:00:10 -0700

    cleaned up paths in tab.router

--- storyTime/src/app/end/end.page.ts
+++ storyTime/src/app/end/end.page.ts
@@ -13,7 +13,7 @@ export class EndPage implements OnInit {
     this.router.navigateByUrl('/middle');
   }
   launchHome() {
-    this.router.navigateByUrl('/tabs/tab1');
+    this.router.navigateByUrl('/tabs/Home');
   }
 
   ngOnInit() {}
--- storyTime/src/app/tabs/tabs.router.module.ts
+++ storyTime/src/app/tabs/tabs.router.module.ts
@@ -8,15 +8,6 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        children: [
-          {
-            path: '',
-            loadChildren: '../tab1/tab1.module#Tab1PageModule'
-          }
-        ]
-      },
-      {
         path: 'home',
         children: [
           {
@@ -25,24 +16,7 @@ const routes: Routes = [
           }
         ]
       },
-      {
-        path: 'tab2',
-        children: [
-          {
-            path: '',
-            loadChildren: '../tab2/tab2.module#Tab2PageModule'
-          }
-        ]
-      },
-      {
-        path: 'tab3',
-        children: [
-          {
-            path: '',
-            loadChildren: '../tab3/tab3.module#Tab3PageModule'
-          }
-        ]
-      },
+
       {
         path: 'info',
         children: [
@@ -54,14 +28,14 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/home',
     pathMatch: 'full'
   }
 ];
